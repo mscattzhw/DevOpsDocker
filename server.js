591ca8aa04bff8a4e824de4620d59d75343d34ae
mscattzhw <146068193+mscattzhw@users.noreply.github.com>
2024-05-19 17:11:34 +0200

    Änderung für test auf Jenkins Übung 12

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello FS2024 DevOps Course! :-))) (((-:');
+	res.send('Hello FS2024 DevOps Course! :-))) (((-: Nun kommen neue Änderungen von Matteo für die Übung 12. ;) XD :-D :-P :-O :-* :-/ :-| :-[ :-] :-{ :-} :-! :-@ :-# :-$ :-^ :-& :-* :-) :-(');
 });
 
 app.listen(PORT, HOST);
