963703b6444e9d1426a3faf94e25bc24760643b8
mscattzhw <146068193+mscattzhw@users.noreply.github.com>
2024-05-19 17:46:28 +0200

    Automatische Änderung zeitlich mit github

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-	res.send('Hello FS2024 DevOps Course! :-))) (((-: Nun kommen neue Änderungen von Matteo für die Übung 12. ;) XD :-D :-P :-O :-* :-/ :-| :-[ :-] :-{ :-} Änderung 22222222222222 :-! :-@ :-# :-$ :-^ :-& :-* :-) :-(');
+	res.send('Dritte Variante die automatisch via Github alles automatisch auslösen soll!Hello FS2024 DevOps Course! :-))) (((-: Nun kommen neue Änderungen von Matteo für die Übung 12. ;) XD :-D :-P :-O :-* :-/ :-| :-[ :-] :-{ :-} Änderung 22222222222222 :-! :-@ :-# :-$ :-^ :-& :-* :-) :-(');
 });
 
 app.listen(PORT, HOST);
